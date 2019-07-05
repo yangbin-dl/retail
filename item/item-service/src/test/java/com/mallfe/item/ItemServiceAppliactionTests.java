4ca8ef3e1548c73f01bd034bdcd8ba48ef9c76d4
drduan <drduan@vip.qq.com>
2019-07-06 00:11:32 +0800

    test

--- item/item-service/src/test/java/com/mallfe/item/ItemServiceAppliactionTests.java
+++ item/item-service/src/test/java/com/mallfe/item/ItemServiceAppliactionTests.java
@@ -1,18 +1,21 @@
 package com.mallfe.item;
 
 
+import com.mallfe.common.vo.PageResult;
+import com.mallfe.item.pojo.Brand;
+import com.mallfe.item.service.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CostCenterApplicationTests {
+public class ItemServiceAppliactionTests {
 	@Autowired
-	private CostSaleMapper costSaleMapper;
+	private BrandService brandService;
 
 	@Test
 	public void contextLoads() {
-		CostSaleData costSale =costSaleMapper.selectByPrimaryKey(1);
-		System.out.println("CostSql = " + costSale);
+		PageResult<Brand> data =brandService.queryBrandByPage(1,5,null,null,null);
+		System.out.println("CostSql = " + data.toString());
 	}
 
 }
